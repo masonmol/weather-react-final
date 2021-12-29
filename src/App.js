f743224d7abebfca9be7d52bd5d9f1c493d96444
masonmol <morganlmason96@gmail.com>
2021-12-29 12:54:23 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This website was coded by{" "}
-        <a
-          href="https://infallible-bhaskara-573893.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Morgan Mason
-        </a>{" "}
-        and open-sourced on{" "}
-        <a href="" target="_blank" rel="noreferrer">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          <button className="btn btn-primary">Hello</button>
+          This website was coded by{" "}
+          <a
+            href="https://infallible-bhaskara-573893.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Morgan Mason
+          </a>{" "}
+          and open-sourced on{" "}
+          <a
+            href="https://github.com/masonmol/weather-react-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
