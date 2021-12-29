006fc181726e1490200a4f1f52f757cb0273ffae
masonmol <morganlmason96@gmail.com>
2021-12-29 12:19:21 -0500

    Customized React app and added footer

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <footer>
+        This website was coded by{" "}
+        <a
+          href="https://infallible-bhaskara-573893.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Morgan Mason
+        </a>{" "}
+        and open-sourced on{" "}
+        <a href="" target="_blank" rel="noreferrer">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
