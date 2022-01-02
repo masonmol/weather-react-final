b9b4911d62dd8092abe9b0be2da39313feaf4984
masonmol <morganlmason96@gmail.com>
2022-01-02 17:41:42 -0500

    updated search engine

--- src/Weather.js
+++ src/Weather.js
@@ -6,6 +6,7 @@ import WeatherInfo from "./WeatherInfo.js";
 import "./Weather.css";
 
 export default function Weather(props) {
+  const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
@@ -26,6 +27,21 @@ export default function Weather(props) {
     });
   }
 
+  function search() {
+    const apiKey = "eda5f4c1faef5ba99e914999cfcb1292";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
@@ -37,7 +53,7 @@ export default function Weather(props) {
             <FormattedTime date={weatherData.time} />
           </div>
         </div>
-        <form className="Search">
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -45,11 +61,11 @@ export default function Weather(props) {
                 placeholder="Type a city..."
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
               <input type="submit" value="Search" className="btn btn-primary" />
-              <i className="fa fas fa-location-arrow"></i>
             </div>
           </div>
         </form>
@@ -57,11 +73,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "eda5f4c1faef5ba99e914999cfcb1292";
-    let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
+    search();
     return "Loading...";
   }
 }
